adb62df31e41d87fceb18d8b1f9b9484409367f7
Maidanets <dimonchyk.ua@gmail.com>
2023-04-24 21:47:45 +0300

    update Panel

--- Panel/urls.py
+++ Panel/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 import Panel.views
 
 urlpatterns = [
-    path('panel/', Panel.views.panel_page),
-    path('panel/booking/', Panel.views.panel_booking),
-    path('panel/specialist/', Panel.views.panel_specialist),
-    path('panel/specialist/<int:id_specialist>/', Panel.views.panel_specialist_id),
+    path('', Panel.views.panel_page),
+    path('booking/', Panel.views.panel_booking),
+    path('services/', Panel.views.panel_services),
+    path('services/<int:id_service>/', Panel.views.panel_service_id),
+    path('masters/', Panel.views.panel_masters),
+    path('masters/<int:id_master>/', Panel.views.panel_master_id),
 ]
--- Panel/views.py
+++ Panel/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from Salon.models import Services, Master, Schedule, Booking
 
 # Create your views here.
 
@@ -12,9 +13,53 @@ def panel_booking(request):
     return HttpResponse("Panel Booking")
 
 
-def panel_specialist(request):
-    return HttpResponse("Panel Specialist")
+def panel_services(request):
+    if request.method == "POST":
+        service = Services(
+            name_service=request.POST['name_service'],
+            time_service=request.POST['time_service'],
+            price_service=request.POST['price_service']
+        )
+        service.save()
+    services = Services.objects.all()
+    return render(request, "panel_services.html", {'services': services})
 
 
-def panel_specialist_id(request, id_specialist):
+def panel_service_id(request, id_specialist):
     return HttpResponse(f"Panel Specialist id = {id_specialist}")
+
+
+def panel_masters(request):
+    # [value for key, value in request.POST.items() if key.startswith('services_master')]
+
+    if request.method == "POST":
+        master = Master(
+            name_master=request.POST['name_master'],
+            phone_master=request.POST['phone_master'],
+            rank_master=request.POST['rank_master'],
+        )
+        master.save()
+
+        service_ids = [value for key, value in request.POST.items() if key.startswith('services_master')]
+        for service_id in service_ids:
+            service = Services.objects.get(id=service_id)
+            master.services_master.add(service)
+        master.save()
+
+    masters = Master.objects.all()
+    services = Services.objects.all()
+    return render(request, "panel_masters.html", {'masters': masters, 'services': services})
+
+
+def panel_master_id(request, id_master):
+    if request.method == "POST":
+        schedule = Schedule(
+            master_id=id_master,
+            date=request.POST['date'],
+            start_work=request.POST['start_work'],
+            end_work=request.POST['end_work']
+        )
+        schedule.save()
+    current_schedule = Master.objects.get(id=id_master)
+    work_schedule = Schedule.objects.filter(master_id=current_schedule).all()
+    return render(request, "panel_master_one.html", {'work_schedule': work_schedule})
